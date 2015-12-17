6ebc6d12c9cff7425f25c3e9dae81e1d5ac0c2ce
郑浩乐 <89932980@qq.com>
2015-12-17 11:25:26 +0800

    updated 添加jsonp方法处理

--- ajax.js
+++ ajax.js
@@ -12,7 +12,9 @@ function ajax(params){
 		error: params.error||function() {},
 		success: params.success||function(){},
 		dataType: params.dataType||'json',
-		data: params.data||{}
+		data: params.data||{},
+		jsonp:'callback',
+		jsonpCallback:('jsonp_' + Math.random()).replace('.','')
 	};	
 	var formatParams = function(json) {
         var arr = [];
@@ -21,43 +23,67 @@ function ajax(params){
         }
         return arr.join("&");
     };
-	options.data = formatParams(options.data);
-
-	var xhr = new (self.XMLHttpRequest||ActiveXObject)("Microsoft.XMLHTTP"); //最简实现xhr
-	if(!xhr){
-		return false;
-	}
-	if (options.type == 'POST') {
-		xhr.open(options.type, options.url, options.async);
-		xhr.setRequestHeader('content-type','application/x-www-form-urlencoded');
-		xhr.send(options.data);
+	if(options.dataType=='jsonp'){
+		//插入动态脚本及回调函数
+		var $head = document.getElementsByTagName('head')[0];
+		var $script = document.createElement('script');
+		$head.appendChild($script);	
+        window[options.jsonpCallback] = function (json) {
+            $head.removeChild($script);
+            window[options.jsonpCallback] = null;
+            options.success(json);
+        };
+        //发送请求
+        options.data[options.jsonp] = options.jsonpCallback;
+        $script.src = options.url + '?' + formatParams(options.data);
+        //超时处理
+        setTimeout(function(){
+            $head.removeChild($script);
+            window[options.jsonpCallback] = null;
+            options.error();
+        }, options.timeout);
 	}else{
-		xhr.open(options.type, options.url + '?'+ options.data, options.async);
-		xhr.send(null);
-	}
-	var requestDone = false;
-	setTimeout(function() {
-		requestDone = true;
-		if(xhr.readyState != 4){
-			xhr.abort();
+		//创建xhr对象
+		var xhr = new (self.XMLHttpRequest||ActiveXObject)("Microsoft.XMLHTTP");
+		if(!xhr){
+			return false;
 		}
-	}, options.timeout);
-	xhr.onreadystatechange = function(){
-		if(xhr.readyState == 4&&!requestDone) {
-			if(xhr.status>=200 && xhr.status<300) {
-				var data = options.dataType == "xml" ? xhr.responseXML : xhr.responseText;
-				if (options.dataType == "json") {
-					try{
-						data =  JSON.parse(data);
-					}catch(e){
-						data = eval('(' + data + ')');
-					}
-				}
-				options.success(data);
-			} else {
-				options.error();
+		//发送请求
+		options.data = formatParams(options.data);
+		if (options.type == 'POST') {
+			xhr.open(options.type, options.url, options.async);
+			xhr.setRequestHeader('content-type','application/x-www-form-urlencoded');
+			xhr.send(options.data);
+		}else{
+			xhr.open(options.type, options.url + '?'+ options.data, options.async);
+			xhr.send(null);
+		}
+		//超时处理
+		var requestDone = false;
+		setTimeout(function() {
+			requestDone = true;
+			if(xhr.readyState != 4){
+				xhr.abort();
 			}
-			options.complete();
-		}
-	};
+		}, options.timeout);
+		//状态处理
+		xhr.onreadystatechange = function(){
+			if(xhr.readyState == 4&&!requestDone) {
+				if(xhr.status>=200 && xhr.status<300) {
+					var data = options.dataType == "xml" ? xhr.responseXML : xhr.responseText;
+					if (options.dataType == "json") {
+						try{
+							data =  JSON.parse(data);
+						}catch(e){
+							data = eval('(' + data + ')');
+						}
+					}
+					options.success(data);
+				} else {
+					options.error();
+				}
+				options.complete();
+			}
+		};		
+	}
 }
