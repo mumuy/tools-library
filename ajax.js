ba5ff7b710fa8b1373947ee1f21022b9e5c3f69d
郑浩乐 <89932980@qq.com>
2015-12-17 10:58:38 +0800

    updated 参数名与jQuery兼容

--- ajax.js
+++ ajax.js
@@ -1,21 +1,17 @@
 function ajax(params){
-    var xhr = new (self.XMLHttpRequest||ActiveXObject)("Microsoft.XMLHTTP"); //最简实现xhr
-	if(!xhr){
-		return false;
-	}
 	params = params||{};
 	if (!params.url || !params.callback) {
-        throw new Error('Necessary parameters are missing'); //必要参数未填
+        throw new Error('Necessary parameters are missing.'); //必要参数未填
     }
 	var options = {
 		url: params.url||'',
-		method: (params.method||'GET').toUpperCase(),
+		type: (params.type||'GET').toUpperCase(),
 		timeout: params.timeout||5000,
 		async : true,
 		complete: params.complete||function(){},
 		error: params.error||function() {},
 		success: params.success||function(){},
-		type: params.type||'json',
+		dataType: params.dataType||'json',
 		data: params.data||{}
 	};	
 	var formatParams = function(json) {
@@ -26,12 +22,17 @@ function ajax(params){
         return arr.join("&");
     };
 	options.data = formatParams(options.data);
-	if (options.method == 'POST') {
-		xhr.open(options.method, options.url, options.async);
+
+	var xhr = new (self.XMLHttpRequest||ActiveXObject)("Microsoft.XMLHTTP"); //最简实现xhr
+	if(!xhr){
+		return false;
+	}
+	if (options.type == 'POST') {
+		xhr.open(options.type, options.url, options.async);
 		xhr.setRequestHeader('content-type','application/x-www-form-urlencoded');
 		xhr.send(options.data);
 	}else{
-		xhr.open(options.method, options.url + '?'+ options.data, options.async);
+		xhr.open(options.type, options.url + '?'+ options.data, options.async);
 		xhr.send(null);
 	}
 	var requestDone = false;
@@ -44,8 +45,8 @@ function ajax(params){
 	xhr.onreadystatechange = function(){
 		if(xhr.readyState == 4&&!requestDone) {
 			if(xhr.status>=200 && xhr.status<300) {
-				var data = options.type == "xml" ? xhr.responseXML : xhr.responseText;
-				if (options.type == "json") {
+				var data = options.dataType == "xml" ? xhr.responseXML : xhr.responseText;
+				if (options.dataType == "json") {
 					try{
 						data =  JSON.parse(data);
 					}catch(e){
