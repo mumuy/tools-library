9a6808c712a1f15d92394e576261d6ae13be1db1
mumuy <zhenghaole1990@126.com>
2016-07-26 11:06:19 +0800

    updated 垫片添加

--- code/ECMAScript5.js
+++ code/ECMAScript5.js
@@ -36,6 +36,13 @@ if (typeof Object.create !== 'function') {
 		return new F();  
     };  
 }
+if (!Object.keys) Object.keys = function(o) {
+	if (o !== Object(o))
+    	throw new TypeError('Object.keys called on a non-object');
+  	var k=[],p;
+  	for (p in o) if (Object.prototype.hasOwnProperty.call(o,p)) k.push(p);
+  	return k;
+}
 
 //数组
 if(!Array.isArray) {
