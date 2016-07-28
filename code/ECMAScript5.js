2b025534ba04da55615f38d3081f41201748f64e
mumuy <zhenghaole1990@126.com>
2016-07-28 14:15:38 +0800

    updated es5 toISOString方法兼容

--- code/ECMAScript5.js
+++ code/ECMAScript5.js
@@ -22,10 +22,31 @@ if(!String.prototype.trim) {
 }
 
 //日期
-if (!Date.now) {
-	Date.now = function now() {
+if (!Date.now){
+	Date.now = function now(){
     	return new Date().getTime();
 	};
+}
+if (!Date.prototype.toISOString){
+    (function(){
+        function pad(number) {
+            var r = String(number);
+            if (r.length === 1) {
+                r = '0' + r;
+            }
+            return r;
+        }
+        Date.prototype.toISOString = function(){
+            return this.getUTCFullYear()
+                + '-' + pad(this.getUTCMonth() + 1)
+                + '-' + pad(this.getUTCDate())
+                + 'T' + pad(this.getUTCHours())
+                + ':' + pad(this.getUTCMinutes())
+                + ':' + pad(this.getUTCSeconds())
+                + '.' + String((this.getUTCMilliseconds()/1000).toFixed(3)).slice(2,5)
+                + 'Z';
+        };
+    }());
 }
 
 //对象
